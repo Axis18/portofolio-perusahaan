5a6c78d07462291abef3ce2dafa62630f2c066db
Muhammad Abdurrahman <ma6239554@gmail.com>
2023-12-26 10:34:05 +0700

    baru

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import Logo from './logo.svg';
 import Logo1 from './logo512.png';
-<<<<<<< HEAD
-=======
 
->>>>>>> 3890817316b0a576895640e9435770ddc9238ca4
 const EmployeeForm = () => {
   // Daftar karyawan beserta informasi mereka
   const employees = [
-    { id: 1, name: 'John Doe', photo: Logo },
-    { id: 2, name: 'Jane Smith', photo: Logo1 },
+    { id: 1, name: 'John Doe ', photo: Logo },
+    { id: 2, name: 'Jane Smith ', photo: Logo1 },
     // Tambahkan karyawan lain di sini
   ];
 
-  const [currentEmployee, setCurrentEmployee] = useState(employees[0]); // Karyawan awal yang ditampilkan
+  const [currentEmployee, setCurrentEmployee] = useState(employees[1]); // Karyawan awal yang ditampilkan
 
   const changeEmployee = (employee) => {
     setCurrentEmployee(employee);
@@ -21,11 +18,11 @@ const EmployeeForm = () => {
 
   return (
     <div>
-      <div className="employee-card">
+      <div className="employee-card absolute center ">
         <img src={currentEmployee.photo} alt={currentEmployee.name} />
         <h2>{currentEmployee.name}</h2>
       </div>
-      <div className="button-container">
+      <div className="button-container absolute hover:text-blue-800  bg-slate-500 text-red-800 ">
         {employees.map((employee) => (
           <button key={employee.id} onClick={() => changeEmployee(employee)}>
             {employee.name}
